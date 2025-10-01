c131211c8bda42eb4604087354505e77d538507f
ast <asteroidable@gmail.com>
2025-10-02 08:49:40 +0900

    jp skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
 use axum::{routing::get, Router};
+use axum::{extract::Path, http::Uri};
 use tower_service::Service;
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(root))
-}
 
 #[event(fetch)]
 async fn fetch(
@@ -15,6 +13,26 @@ async fn fetch(
     Ok(router().call(req).await?)
 }
 
+
+fn router() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/jp/{input}/s/{*url}", get(jp))
+}
+
+
 pub async fn root() -> &'static str {
-    "Hello Axum!"
+    "dataquery"
+}
+
+/// /jp/{input}/s/{url}
+pub async fn jp(Path((input, url)): Path<(String, String)>, uri: Uri) -> String {
+    let full_url = match uri.query() {
+        Some(q) if !q.is_empty() => {
+            let sep = if url.contains('?') { '&' } else { '?' };
+            format!("{url}{sep}{q}")
+        },
+        _ => url,
+    };
+    format!("{input}\n{full_url}")
 }
